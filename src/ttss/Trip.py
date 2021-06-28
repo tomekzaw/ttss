70d9cba0847542f20d70881d82edb6be0f5cfe6f
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2021-06-28 23:02:01 +0200

    Parse stop, trip and route fields

--- src/ttss/Trip.py
+++ src/ttss/Trip.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from ttss.Route import Route
+
 
 @dataclass
 class Trip:
     id: Optional[str] = None
-    route_number: Optional[str] = None
+    route: Optional[Route] = None
     direction: Optional[str] = None
