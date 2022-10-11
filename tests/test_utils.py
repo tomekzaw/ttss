7c06ef19ef8c30bfa02cce3b8bbac35c8353d14e
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-10-11 18:25:07 +0200

    Add `timestamp_ms` test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,14 @@
 from datetime import time
 
-from ttss.utils import parse_time
+import pytest
+
+from ttss.utils import parse_time, timestamp_ms
 
 
 def test_parse_time():
     assert parse_time('12:34') == time(12, 34)
+
+
+@pytest.mark.freeze_time('2021-06-28 21:33:19')
+def test_timestamp_ms():
+    assert timestamp_ms() == 1624915999000
