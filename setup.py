b40bc82fb93b62cb9e9e051bc6993df26af78370
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-08-28 19:21:24 +0200

    Release 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='ttss',
-    version='0.6.0',
+    version='0.7.0',
     author='Tomek Zawadzki',
     author_email='tomekzawadzki98@gmail.com',
     description='A Python wrapper for TTSS API.',
